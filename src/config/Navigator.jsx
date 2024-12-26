85f0ae511e8a8869bb0c27fa993401e9390439e6
Alishaqureshi2005 <alishaqureshi227@gmail.com>
2024-12-26 16:49:00 +0500

    update

--- src/config/Navigator.jsx
+++ src/config/Navigator.jsx
@@ -4,8 +4,8 @@ import { Home, Page2, Page3 } from "../pages"
 const Navigator = () => {
   return (
     <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/page2" element={<Page2 />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Page2 />} />
         <Route path="/page3" element={<Page3 />} />
     </Routes>
   )
